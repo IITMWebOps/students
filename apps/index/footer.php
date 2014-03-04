90288fa091f21f7a1fc377c3878e3189f10854aa
prasanthsai <yashmurty@gmail.com>
2014-03-05 00:00:06 +0530

    Social Icons - Footer

--- apps/index/footer.php
+++ apps/index/footer.php
@@ -1,3 +1,47 @@
 <div class="small-12 columns" id="Footer-Body">                               	
           <p>Copyrights - Institute WebOps</p>
+          
+				
+				<div id="social-icons">
+									<ul>
+										<li class="social-icon twitter">
+											<a class="fade-img" href="https://twitter.com/reachIITM" rel="tooltip" title="Twitter">
+												<img src="../../../spapp/assets/images/social/icons_twitter.png" alt="Twitter">
+											</a>
+										</li>
+										<li class="social-icon facebook">
+											<a class="fade-img" href="https://www.facebook.com/ReachIITM" rel="tooltip" title="Facebook">
+												<img src="../../../spapp/assets/images/social/icons_facebook.png" alt="Facebook">
+											</a>
+										</li>
+										<li class="social-icon gplus">
+											<a class="fade-img" href="https://plus.google.com/+iitmadras/posts" rel="tooltip" title="Google+">
+												<img src="../../../spapp/assets/images/social/icons_google.png" alt="Google+">
+											</a>
+										</li>
+										
+										<li class="social-icon youtube">
+											<a class="fade-img" href="https://www.youtube.com/user/ReachIITM?feature=watch" rel="tooltip" title="YouTube">
+												<img src="../../../spapp/assets/images/social/icons_youtube.png" alt="YouTube">
+											</a>
+										</li>
+										<li class="social-icon linkedin">
+											<a class="fade-img" href="http://www.linkedin.com/edu/school?id=13501&goback=%2Epts_13501_*1_*1_*1_*1&trk=edu-cp-title" rel="tooltip" title="LindekIn">
+												<img src="i../../../spapp/assets/images/social/icons_linkedin.png" alt="LindekIn">
+											</a>
+										</li>
+										<li class="social-icon flickr">
+											<a class="fade-img" href="http://www.flickr.com/groups/iit-madras/" rel="tooltip" title="Flickr">
+												<img src="../../../spapp/assets/images/social/icons_flickr.png" alt="Flickr">
+											</a>
+										</li>
+										
+										<li class="social-icon rss">
+											<a class="fade-img" href="http://t5e.iitm.ac.in/" rel="tooltip" title="T5E">
+												<img src="../../../spapp/assets/images/social/icons_rss.png" alt="T5E">
+											</a>
+										</li>
+									</ul>
+								</div>				
+				          
 </div>
