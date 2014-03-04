be6a37a48d9f7401e44a3f4b095c90cf1b56903f
prasanthsai <yashmurty@gmail.com>
2014-03-05 00:51:38 +0530

    Minor Change - Left Sidebar

--- apps/index/left-sidebar.php
+++ apps/index/left-sidebar.php
@@ -5,11 +5,11 @@
         	<ul class="side-nav">
             	<li><form>
     			<!--  <label>Search Documentation</label> -->
-    			<input tabindex="1" id="" type="search" placeholder="Search Students:" ng-model="StudentSearch.search" ng-change = "app.request({location: '/student_search?search='+StudentSearch.search, progress: false, alert: false, method: 'POST', changeURL: true })" ng-focus = "app.request({location: '/student_search?search='+StudentSearch.search, progress: false, alert: false, method: 'POST', changeURL: true })" autocomplete="off">
+    			<input tabindex="1" id="" type="search" placeholder="Search for Students | Eg: Yash" ng-model="StudentSearch.search" ng-change = "app.request({location: '/student_search?search='+StudentSearch.search, progress: false, alert: false, method: 'POST', changeURL: true })" ng-focus = "app.request({location: '/student_search?search='+StudentSearch.search, progress: false, alert: false, method: 'POST', changeURL: true })" autocomplete="off">
   			</form></li>
   				<li><a href="#">Home</a></li>
   				<li><a href="#">Elections 2014</a></li>
-  				<li><a href="#">Landing Page</a></li>
+  				<li><a href="http://students.iitm.ac.in/">Landing Page</a></li>
   				<li><a href="#">Contact Us</a></li>
   				
 			</ul>
