ecb68d4487a42809b4902f096a674995ed97516e
prasanthsai <yashmurty@gmail.com>
2014-03-08 10:54:15 +0530

    Application search

--- apps/search/index.php
+++ apps/search/index.php
@@ -7,10 +7,22 @@ if( !$current_user->login() ) redirect_to('/user/login', true);
 $data->search = $_GET['q'];
 $limit_f = isset( $_GET['f']) ? $_GET['f'] : 0;
 
+if($limit_f){
+	$limit_f1=$limit_f;
+	$limit_f2=$limit_f;
+	$limit_f3=$limit_f;}
+else{
+	$limit_f1 = $limit_f;
+	$limit_f2 = $limit_f - $count_pages_result[0];
+	$limit_f3 = $limit_f - $count_pages_result[0] - $count_applications_result[0];
+}
+
+$limit1 = 10;
+
 $query_pages = "SELECT DISTINCT name,link,content,trash 
 		  FROM `stu_portal`.`pages` 
 		  WHERE (name like '%$data->search%' or link like '%$data->search%') and (trash !=1)
-		  LIMIT ".($limit_f-$count_students_result[0]-$count_applications_result[0]).", 10";
+		  LIMIT ".$limit_f1.", ".$limit1;
 $result_pages = mysql_query($query_pages);
 $count_pages = mysql_query("SELECT COUNT(id) 
 					  FROM stu_portal.pages 
@@ -19,10 +31,11 @@ $count_pages = mysql_query("SELECT COUNT(id)
 $count_pages_result = mysql_fetch_array($count_pages);
 echo "<script>alert(".$count_pages_result[0].");</script>";
 
+$limit2 = $limit1 - $count_pages_result[0];
 $query_applications = "SELECT DISTINCT name,link 
 		  FROM `stu_portal`.`applications` 
 		  WHERE name like '%$data->search%'
-		  LIMIT ".($limit_f-$count_pages_result[0]).", ". (10-$count_pages_result[0]);
+		  LIMIT ".$limit_f2.", ". $limit2;
 $result_applications = mysql_query($query_applications);
 $count_applications = mysql_query("SELECT COUNT(id) 
 					  FROM stu_portal.applications 
@@ -31,10 +44,11 @@ $count_applications = mysql_query("SELECT COUNT(id)
 $count_applications_result = mysql_fetch_array($count_applications);
 
 
+$limit3 = $limit1 - $count_pages_result[0] - $count_applications_result[0];
 $query_students = "SELECT DISTINCT username,fullname,room,hostel,contact 
 		  FROM `stu_portal`.`users` 
 		  WHERE username like '%$data->search%' or fullname like '%$data->search%' or email like '%$data->search%' or contact like '%$data->search%'
-		  LIMIT ".$limit_f.", ".(10-$count_pages_result[0]-$count_applications_result[0]);
+		  LIMIT ".$limit_f3.", ".$limit3;
 $result_students = mysql_query($query_students);
 $count_students = mysql_query("SELECT COUNT(id) 
 					  FROM stu_portal.users 
