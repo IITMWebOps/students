e0f68db5931a3661110f41e9174073b751b4720b
prasanthsai <yashmurty@gmail.com>
2014-03-10 00:20:17 +0530

    file upload modified mand

--- apps/file_upload/upload.php
+++ apps/file_upload/upload.php
@@ -13,30 +13,36 @@ if ((($_FILES["file"]["type"] == "image/gif")
   {
 if ($_FILES["file"]["error"] > 0)
   {
+   echo "{'error' : '".$_FILES["file"]["error"]."'}";
   
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
+  
   }
 else
   {
   
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+ 
   }
+   $random_string = substr( "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", mt_rand(0, 50) , 15) .substr( md5( time() ), mt_rand(0,12), 15) ;
+      $_FILES["file"]["name"]=$random_string.$extension;
    if (file_exists($_FILES["file"]["name"]))
       {
-      echo $_FILES["file"]["name"] . " already exists. ";
+       echo "{'error' : '"."file already exists"."'}";
       }
     else
       {
-      move_uploaded_file($_FILES["file"]["tmp_name"],FILE_ROOT."/".$_FILES["file"]["name"]);
-      echo "Stored as: " . $_FILES["file"]["name"];
+        echo "{'name' : '".$_FILES["file"]["name"]."'}";
+
+
+      if(move_uploaded_file($_FILES["file"]["tmp_name"],FILE_DIR_ROOT."/".$_FILES["file"]["name"]))
+      echo "test";
+      
       }
   }
   else
   {
-  echo "Invalid file";
-  print_r( $_FILES["file"]["error"] );
+   echo "{'error' : '"."wrong file type or file too large"."'}";
+  
   }
+  $query= "INSERT INTO `stu_portal`.`attachments` (`filename`,`created_by`,`created_at`) VALUES('".$_FILES["file"]["name"]."','".$current_user->id."', CURRENT_TIMESTAMP)";
+  $result=mysql_query($query)or trigger_error(mysql_error());
 ?>
